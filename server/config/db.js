1b7c65286a5dca06e250973395aae51ae2bd6933
Shapira568 <chikahappiness589@gmail.com>
2026-05-28 12:32:46 +0100

    Fix Aiven SSL certificate issue

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,8 @@
 const { Sequelize } = require('sequelize');
 
+const pg = require('pg');
+pg.defaults.ssl = { rejectUnauthorized: false };
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
