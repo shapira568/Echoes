92860efb13c0274e9899125b8eaff06fd3150cfb
Shapira568 <chikahappiness589@gmail.com>
2026-05-28 12:39:36 +0100

    Force disable TLS rejection for Aiven SSL

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,6 @@
-const { Sequelize } = require('sequelize');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-const pg = require('pg');
-pg.defaults.ssl = { rejectUnauthorized: false };
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
